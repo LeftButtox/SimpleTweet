796479e7be14f07ccd12a89eb48f19fdee6527b4
Alexander Le <alexanvl@uci.edu>
2021-01-31 23:36:42 -0800

    added menu icon, compose button, compose screen, post to twitter, and get last 5 tweets from offline

--- SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,32 +1,58 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.provider.ContactsContract;
 import android.util.Log;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
 import com.codepath.apps.restclienttemplate.TimeFormatter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-
+@Parcel
+@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "userId"))
 public class Tweet {
-    public String body;
-    public static String createdAt;
+
+    @PrimaryKey
+    @ColumnInfo
     public long id;
+
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String createdAt;
+
+    @ColumnInfo
+    public long userId;
+
+    @Ignore
     public User user;
     public static final String TAG = "Tweet.java";
+
+    // empty constructor needed by the parceler library
+    public Tweet(){}
+
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.id = jsonObject.getLong("id");
-        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
-        Log.i(TAG, "createdAt: " + tweet.createdAt);
+        User user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.user = user;
+        tweet.userId = user.id;
         return tweet;
     }
 
@@ -38,7 +64,7 @@ public class Tweet {
         return tweets;
     }
 
-    public static String getFormattedTimestamp(){
+    public String getFormattedTimestamp(){
         Log.i("TWEET.java", "DATE: " + createdAt);
         return TimeFormatter.getTimeDifference(createdAt);
     }
